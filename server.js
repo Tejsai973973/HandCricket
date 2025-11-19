f8f01247e70496ea950f7c36fe44f49c969c54d5
Tejsai <tejsai19730106@gmail.com>
2025-11-19 12:42:43 +0530

    v2.1

--- server.js
+++ server.js
@@ -1045,6 +1045,8 @@ io.on('connection', (socket) => {
     });
 });
 
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}!`);
     if (process.env.NODE_ENV === 'production') {
